b45988c24287c1a6f8eac40d97b1bb8692d0c9a3
armin bakhshi <arminbkh0921@yahoo.com>
2021-10-01 00:05:05 +0330

    develope GroupItem component

--- src/Components/ManageFiltersGroups/Group/Group.jsx
+++ src/Components/ManageFiltersGroups/Group/Group.jsx
@@ -1,15 +1,13 @@
 import { useEffect } from "react";
+import GroupItem from "./GroupItem/GroupItem";
 
 const Group = ({ title, group }) => {
-    useEffect(()=>{
-        console.log(group)
-    }, [])
     return ( 
-        <article>
-            <h1 className={`text-gray-9000 font-bold text-2xl my-5 Dirooz`}>دسته بندی {title}</h1>
+        <article className={`boxShadow rounded-md p-5 mt-5`}>
+            <h1 className={`text-gray-9000 font-bold text-2xl mb-5 Dirooz`}>دسته بندی {title}</h1>
             <ul>
                 {
-                    group.map((it => <li key={it.id}>{it.label}</li>))
+                group.map(((it, index) => <GroupItem key={it.id} index={index + 1} label={it.value} />))
                 }
             </ul>
         </article>
---
+++ src/Components/ManageFiltersGroups/Group/GroupItem/GroupItem.jsx
@@ -0,0 +1,29 @@
+import { BiTrash } from "react-icons/bi";
+
+const GroupItem = ({ label, type, index }) => {
+  const removeFilterItemHandler = async () => {
+      try{
+        
+      } catch(err){
+
+      }
+  };
+  return (
+    <li
+      className={`px-3 flex items-center justify-between Dirooz text-gray-700 text-lg font-medium border-b border-blue-300`}
+    >
+      <div className={`flex items-center`}>
+        <span className={`text-blue-400 p-2 ml-5`}>{index}</span>
+        <p className={`py-4`}>{label}</p>
+      </div>
+      <button
+        onClick={removeFilterItemHandler}
+        className={`p-2 rounded-full bg-blue-400 text-white`}
+      >
+        <BiTrash />
+      </button>
+    </li>
+  );
+};
+
+export default GroupItem;
