e4eab297f4ffd94c701e0b8ba50323471a5cc32d
Armin Bakhshi <alireza@Alirezas-MacBook-Air.local>
2021-10-11 18:06:08 +0330

    use formik and yup for validation loginForm component

--- src/Components/Common/AuthInput/AuthInput.jsx
+++ src/Components/Common/AuthInput/AuthInput.jsx
@@ -1,7 +1,7 @@
 import { useRef, useState } from 'react';
 import { AiFillEye, AiFillEyeInvisible } from 'react-icons/ai';
 
-const AuthInput = ({ type, ...rest }) => {
+const AuthInput = ({ type, formik, name, ...rest }) => {
     const [isShow, setIsShow] = useState(false);
     const [theType, setTheType] = useState(type);
     const inputRef = useRef();
@@ -14,8 +14,12 @@ const AuthInput = ({ type, ...rest }) => {
     }
 
     return ( 
-        <fieldset className={`relative`}>
-        <input ref={inputRef} className={`w-full Casablanca px-3 border border-transparent focus:border-blue-500 py-1
+        <fieldset className={`flex flex-col`}>
+        {formik.errors[name] && formik.touched[name] && (
+        <span className={`text-xs md:text-hg text-red-700 Dirooz self-end mb-2`}>{formik.errors[name]}</span>
+        )}
+        <div  className={`relative`}>
+        <input ref={inputRef} name={name} className={`w-full Casablanca px-3 border border-transparent focus:border-blue-500 py-1
          boxShadow mb-5 Dirooz bgLight rounded-md outline-none placeholder-gray-500`} type={theType} {...rest} />
          {
             type === "password" &&
@@ -23,6 +27,7 @@ const AuthInput = ({ type, ...rest }) => {
             {!isShow ? <AiFillEye /> : <AiFillEyeInvisible />}
          </span>
          }
+        </div>
         </fieldset>
      );
 }
--- src/Components/auth/LogInForm/LogInForm.jsx
+++ src/Components/auth/LogInForm/LogInForm.jsx
@@ -3,38 +3,30 @@ import { Link, withRouter } from "react-router-dom";
 import AuthInput from "../../Common/AuthInput/AuthInput";
 import { getAllPersonnel } from '../../../Services/getAllPersonnel';
 import { getEmployee } from '../../../Services/getEmployee';
+import { useFormik } from "formik";
+import * as Yup from 'yup';
+
+const initialValues = {
+  userName: '',
+  userPassword: '',
+}
+
+const validationSchema = Yup.object({
+  userName: Yup.string().required('نام کاربری را وارد کنید'),
+  userPassword: Yup.string().required('رمز ورود را وارد کنید'),
+})
 
 const LogInForm = ({ history }) => {
-  const [formValue, setFormValue] = useState({
-    userName: "",
-    userPassword: "",
-  });
-  const [error, setError] = useState('');
 
-  const changeHandler = (e) => {
-    setError('');
-    setFormValue({
-      ...formValue,
-      [e.target.name]: e.target.value,
-    });
-  };
-
-  const submitHandler = async (e) => {
-    e.preventDefault();
-    if(formValue.userName || formValue.userPassword){
-      if(formValue.userName){
-        if(formValue.userPassword){
+  const onSubmit = async (values) => {
           try{
             const { data } = await getAllPersonnel();
-            if(data.find(em => em.id === formValue.userPassword && em.name === formValue.userName)){
-              const { data } = await getEmployee(formValue.userPassword);
+            if(data.find(em => em.id === values.userPassword && em.name === values.userName)){
+              const { data } = await getEmployee(values.userPassword);
               localStorage.setItem('restaurantUser', JSON.stringify(data));
               history.push('/manage', { employee: data })
             } else setError('حساب کاربری مورد نظر یافت نشد')
           } catch(err) {  }
-        } else  setError('کد ورود ضروری است') 
-      } else  setError('نام کاربری ضروری است') 
-    } else  setError('نام کاربری و کد ورود ضروری است') 
     // try{
     //   const { data } = await getAllPersonnel();
     //   if(data.find(em => em.id === formValue.userPassword && em.name === formValue.userName)){
@@ -45,24 +37,36 @@ const LogInForm = ({ history }) => {
     // } catch(err) {}
   };
 
+  const formik = useFormik({
+    onSubmit,
+    initialValues,
+    validationSchema,
+    validateOnMount: true,
+  })
+  const [error, setError] = useState('');
+
   return (
-    <form className={`boxShadow p-5 rounded-3xl w-full md:max-w-lg lg:max-w-xl`} onSubmit={submitHandler}>
+    <form className={`boxShadow p-5 rounded-3xl w-full md:max-w-lg lg:max-w-xl`} onSubmit={formik.handleSubmit}>
       <h1
         className={`text-blue-400 font-bold text-lg  lg:text-4xl Casablanca mb-7 lg:mb-16`}
       >
         ورود
       </h1>
       <AuthInput
+      formik={formik}
         placeholder="نام کاربری..."
-        value={formValue.userName}
-        onChange={changeHandler}
+        value={formik.values.userName}
+        onChange={formik.handleChange}
+        onBlur={formik.handleBlur}
         name="userName"
         type="text"
       />
       <AuthInput
+      formik={formik}
         placeholder="کد ورود..."
-        value={formValue.userPassword}
-        onChange={changeHandler}
+        value={formik.values.userPassword}
+        onChange={formik.handleChange}
+        onBlur={formik.handleBlur}
         name="userPassword"
         type="password"
       />
@@ -75,7 +79,8 @@ const LogInForm = ({ history }) => {
       </Link>
       <button
         type="submit"
-        className={`mt-9 lg:mt-14 block w-full py-2 rounded-md Casablanca tracking-widest text-lg gradient text-white`}
+        className={`mt-9 lg:mt-14 block w-full py-2 rounded-md Casablanca tracking-widest text-lg  ${formik.isValid ? 'gradient text-white' : 'text-blue-400 border-2 border-blue-400 opacity-40' }`}
+        disabled={!formik.isValid}
       >
         ورود
       </button>
