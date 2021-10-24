1022289e896085d57eb756b04f5d2fdec77f0b13
armin-bkh <arminbkh0921@yahoo.com>
2021-10-24 16:27:16 +0330

    remove useless packages (form-data, sweetalert2-react-content)

--- src/Services/postProduct.js
+++ src/Services/postProduct.js
@@ -1,10 +1,10 @@
 import http from "./httpServices";
 
-export function postProduct(data){
-    const header = {
-        headers: {
-            "Content-Type": "multipart/form-data"
-        }
-    }
-    return http.post("/products", data, header)
+export function postProduct(data) {
+  const header = {
+    headers: {
+      "Content-Type": "multipart/form-data",
+    },
+  };
+  return http.post("/products", data, header);
 }
